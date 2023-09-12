89b7dc5e116e7d84151709bed73a0854be78db2f
X1der1337 <94172826+X1der1337@users.noreply.github.com>
2023-09-12 22:19:07 +0900

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./server/server');
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-
